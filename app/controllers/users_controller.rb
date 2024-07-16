f6ca5e08e209f58e07bc1138bbfe783900b57b09
EC2 Default User <ec2-user@ip-172-31-41-234.ap-northeast-1.compute.internal>
2024-07-16 14:28:07 +0000

    add/userページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
+    @profile_image = @user.profile_image
+    @posts = @user.posts
   end
 
   def edit
@@ -8,14 +12,27 @@ class UsersController < ApplicationController
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to top
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :name, :account)
+    params.require(:user).permit(:email, :telephone_number, :password, :name, :account, :comment, profile_image: []).merge(user_id: current_user.id)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to posts_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :profile_image
+  has_many:posts
+
 # フォローの記述
   # フォローしている関連付け
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
@@ -27,5 +30,13 @@ class User < ApplicationRecord
   # 指定したユーザーをフォローしているかどうかを判定
   def following?(user)
     followings.include?(user)
+  end
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image2.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
   end
 end
