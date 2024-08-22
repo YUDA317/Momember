33361678fe2b4fab17596c21e7fb92cbe25859b7
EC2 Default User <ec2-user@ip-172-31-41-234.ap-northeast-1.compute.internal>
2024-08-22 12:57:45 +0000

    map.js/add

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,37 +1,31 @@
-<div id='map' style="height: 800px;"></div>
-<script>
+/*global google*/
+
 document.addEventListener('turbolinks:load', function() {
-  let map
-  let geocoder
-  const display = document.getElementById('display')
-});
+  let map;
+  let geocoder;
+  const display = document.getElementById('display');
 
   function initMap(){
-    geocoder = new google.maps.Geocoder()
-
+    geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 35.6803997, lng: 139.7690174 },
       zoom: 10,
     });
 
-    marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position:  {lat: 40.7828, lng:-73.9653},
       map: map
     });
 
-  google.maps.event.addListener(map, 'click', mylistener);
+    google.maps.event.addListener(map, 'click', mylistener);
 
-      //クリックしたときの処理
     function mylistener(event){
-      map_lat = document.getElementById('post_lat')
-      map_lng = document.getElementById('post_lng')
-      //markerの位置を設定
-      //event.latLng.lat()でクリックしたところの緯度を取得
+      // let map_lat = document.getElementById('post_lat');
+      // let map_lng = document.getElementById('post_lng');
       marker.setPosition(new google.maps.LatLng(event.latLng.lat(), event.latLng.lng()));
-      //marker設置
       marker.setMap(map);
-      map_lat.value = event.latLng.lat();
-      map_lng.value = event.latLng.lng();
+      // map_lat.value = event.latLng.lat();
+      // map_lng.value = event.latLng.lng();
     }
   }
 
@@ -45,11 +39,12 @@ document.addEventListener('turbolinks:load', function() {
             map: map,
             position: results[0].geometry.location
         });
-        // 下の1行を追加
-        display.textContent = "検索結果：" + results[ 0 ].geometry.location
+        display.textContent = "検索結果：" + results[ 0 ].geometry.location;
       } else {
         alert('該当する結果がありませんでした：' + status);
       }
     });
   }
-</script>
+
+  initMap(); // initMap関数を呼び出す
+});
